dc8adbe8b54a954ea10f415242c6150bfcda3be6
Kevin Lin <kevin.lin.p@gmail.com>
2024-12-06 01:08:54 -0800

    04

--- 04/solution_part1.rb
+++ 04/solution_part1.rb
@@ -1,4 +1,4 @@
-def get_down_slope_diagonal(grid:, x:, y:)
+def down_slope_diagonal(grid:, x:, y:)
   diagonal = []
   while y < grid.length && x < grid[0].length
     diagonal << grid[y][x]
@@ -8,7 +8,7 @@ def get_down_slope_diagonal(grid:, x:, y:)
   diagonal.join
 end
 
-def get_up_slope_diagonal(grid:, x:, y:)
+def up_slope_diagonal(grid:, x:, y:)
   diagonal = []
   while y >= 0 && x < grid[0].length
     diagonal << grid[y][x]
@@ -18,19 +18,19 @@ def get_up_slope_diagonal(grid:, x:, y:)
   diagonal.join
 end
 
-def get_diagonals(grid:)
+def diagonals(grid:)
   down_slope_diagonals = []
   up_slope_diagonals = []
 
   grid.length.times do |y|
-    down_slope_diagonals << get_down_slope_diagonal(grid:, x: 0, y:)
-    up_slope_diagonals << get_up_slope_diagonal(grid:, x: 0, y:)
+    down_slope_diagonals << down_slope_diagonal(grid:, x: 0, y:)
+    up_slope_diagonals << up_slope_diagonal(grid:, x: 0, y:)
   end
 
   grid[0].length.times do |x|
     next if x == 0
-    down_slope_diagonals << get_down_slope_diagonal(grid:, x:, y: 0)
-    up_slope_diagonals << get_up_slope_diagonal(grid:, x:, y: grid.length - 1)
+    down_slope_diagonals << down_slope_diagonal(grid:, x:, y: 0)
+    up_slope_diagonals << up_slope_diagonal(grid:, x:, y: grid.length - 1)
   end
 
   # pp({
@@ -41,7 +41,7 @@ def get_diagonals(grid:)
   down_slope_diagonals + up_slope_diagonals
 end
 
-def get_columns(grid:)
+def columns(grid:)
   grid[0].length.times.map do |x|
     grid.map do |row|
       row[x]
@@ -49,7 +49,7 @@ def get_columns(grid:)
   end
 end
 
-def get_rows(grid:)
+def rows(grid:)
   grid.map do |row|
     row.join
   end
@@ -62,23 +62,23 @@ grid = input.split.map do |line|
   line.chars
 end
 
-# pp get_rows(grid:)
-# pp get_columns(grid:)
-# pp get_diagonals(grid:)
+# pp rows(grid:)
+# pp columns(grid:)
+# pp diagonals(grid:)
 
 count = 0
 
-get_rows(grid:).each do |row|
+rows(grid:).each do |row|
   count += row.scan('XMAS').length
   count += row.scan('SAMX').length
 end
 
-get_columns(grid:).each do |column|
+columns(grid:).each do |column|
   count += column.scan('XMAS').length
   count += column.scan('SAMX').length
 end
 
-get_diagonals(grid:).each do |diagonal|
+diagonals(grid:).each do |diagonal|
   count += diagonal.scan('XMAS').length
   count += diagonal.scan('SAMX').length
 end
