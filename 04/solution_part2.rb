c2243204a356b1b4287c1e1f29624ab91a0dfff3
Kevin Lin <kevin.lin.p@gmail.com>
2024-12-06 19:21:41 -0800

    04
    refactor to symbols

--- 04/solution_part2.rb
+++ 04/solution_part2.rb
@@ -2,33 +2,33 @@ file = File.open(ARGV[0])
 input = file.read
 
 grid = input.split.map do |line|
-  line.chars
+  line.chars.map(&:to_sym)
 end
 
 count = 0
 
+MAS_CORNERS = [[:M, :S], [:S, :M]]
+
 (1..grid.length - 2).each do |y|
   (1..grid[0].length - 2).each do |x|
-    if grid[y][x] == 'A'
-      downslope_corner_coords = [
-        [x - 1, y - 1],
-        [x + 1, y + 1]
-      ]
+    next unless grid[y][x] == :A
 
-      upslope_corner_coords = [
-        [x - 1, y + 1],
-        [x + 1, y - 1]
-      ]
+    downslope_coords = [
+      [x - 1, y - 1],
+      [x + 1, y + 1]
+    ]
 
-      x_mas = [downslope_corner_coords, upslope_corner_coords].all? do |corner_coords|
-        letters = corner_coords.map do |coords|
-          grid[coords[1]][coords[0]]
-        end
+    upslope_coords = [
+      [x - 1, y + 1],
+      [x + 1, y - 1]
+    ]
 
-        letters == ['M', 'S'] || letters == ['S', 'M']
+    count += 1 if [downslope_coords, upslope_coords].all? do |coords|
+      letters = coords.map do |coord|
+        grid[coord[1]][coord[0]]
       end
 
-      count += 1 if x_mas
+      MAS_CORNERS.include?(letters)
     end
   end
 end
